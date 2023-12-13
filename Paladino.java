1e530532331bf7dfaa673e1ca8cd3af127ef548e
Luis Fernando Costa e Lopes <92496285+LuisFF23@users.noreply.github.com>
2023-12-13 16:27:04 -0300

    Add files via upload

---
+++ Paladino.java
@@ -0,0 +1,71 @@
+
+public class Paladino {
+
+	int Level;
+	int Skill;
+	int Atack;
+	int Dextr;
+	int Magic;
+	int Sharp;
+	int Fasth;
+
+	public void SetLevel (int Lvl) {
+	    this.Level = Lvl;	
+	}
+	
+	public int GetLevel () {
+		return this.Level;
+	}
+	
+	public void SetSkill (int Skl) {
+	    this.Skill = Skl;	
+	}
+	
+	public int GetSkill () {
+		return this.Skill;
+	}
+	
+	public void SetAtack (int Atk) {
+	    this.Atack = Atk;	
+	}
+	
+	public int GetAtack () {
+
+		return this.Atack;
+	}
+	
+	public void SetDextr (int Dxt) {
+	    this.Dextr = Dxt;	
+	}
+	
+	public int GetDextr () {
+		return this.Dextr;
+	}
+	
+	public void SetMagic (int Mgc) {
+	    this.Magic = Mgc;	
+	}
+	
+	public int GetMagic () {
+
+		return this.Magic;
+	}
+	
+	public void SetSharp (int Shp) {
+	    this.Sharp = Shp;	
+	}
+	
+	public int GetSharp () {
+		return this.Sharp;
+	}	
+	
+	public void SetFasth (int Fhs) {
+	    this.Fasth = Fhs;	
+	}
+	
+	public int GetFasth () {
+		return this.Fasth;
+	}
+	
+	
+}
